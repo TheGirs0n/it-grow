c9e374a3a2ceb8c177b3259b0770bf095aa7b33c
Timur <huntertring87@gmail.com>
2026-01-25 15:41:17 +0300

    5765gbhj

--- Assets/Resources/PlateResource.gd
+++ Assets/Resources/PlateResource.gd
@@ -2,12 +2,13 @@ extends Resource
 class_name PlantResource
 
 @export_multiline var plant_name : String
+@export_multiline var plant_cool_name : String
 @export var plant_energy : String
 @export_multiline var plant_description : String
 @export var plant_grow_stage_textures : Array[CompressedTexture2D]
 @export_multiline var plant_smell : String
+@export_multiline var plant_additional_property : String
 @export var plant_leaf : CompressedTexture2D
 @export var plant_juice_density : CompressedTexture2D
 @export var plant_juice_color : String
-@export_multiline var plant_additional_property : String
 @export var plant_care_stages : Array[GlobalEnums.PLANT_CARE_TYPE]
--- Assets/Scripts/Plant/PlantTemplate.gd
+++ Assets/Scripts/Plant/PlantTemplate.gd
@@ -5,6 +5,7 @@ class_name PlantTemplate
 @export var plant_circle : FindBoxCircleUI
 
 var plant_name : String
+var plant_cool_name : String
 var plant_energy : String
 var plant_description : String
 var plant_grow_stage_textures : Array[CompressedTexture2D]
