877a9bd6463bcae81d1f2073b26d858db7a9618f
Timur <huntertring87@gmail.com>
2026-02-05 19:33:15 +0300

    FIX settings

--- Assets/Scripts/Global/SettingManager.gd
+++ Assets/Scripts/Global/SettingManager.gd
@@ -3,6 +3,19 @@ extends Node
 var config_file : ConfigFile
 var config_path : String = "user://settings.cfg"
 
+var default_master_volume : float = 50.0
+var default_sfx_volume : float = 50.0
+var default_music_volume : float = 50.0
+
+var default_is_master_volume_mute : bool = false
+var default_is_sfx_volume_mute : bool = false
+var default_is_music_volume_mute : bool = false
+
+var default_window_mode : int = 1
+var default_resolution_mode : int = 0
+
+var default_language : String = "EN"
+
 func _enter_tree() -> void:
 	load_setting_at_startup()
 
@@ -16,27 +29,46 @@ func load_setting_at_startup():
 	apply_setting_from_config()
 	
 func apply_setting_from_config():
-	var master_volume : float = config_file.get_value("audio", "master_volume", 50.0)
-	var sfx_volume : float = config_file.get_value("audio", "sfx_volume", 50.0)
-	var music_volume : float = config_file.get_value("audio", "music_volume", 50.0)
+	var master_volume : float = config_file.get_value("audio", "master_volume", default_master_volume)
+	var sfx_volume : float = config_file.get_value("audio", "sfx_volume", default_sfx_volume)
+	var music_volume : float = config_file.get_value("audio", "music_volume", default_music_volume)
 	
-	var window_mode : int = config_file.get_value("video", "window_mode", 1)
-	var resolution_mode : int = config_file.get_value("video", "resolution", 0)
+	var is_master_volume_mute : bool = config_file.get_value("audio", "mute_master_volume", default_is_master_volume_mute)
+	var is_sfx_volume_mute : bool = config_file.get_value("audio", "mute_sfx_volume", default_is_sfx_volume_mute)
+	var is_music_volume_mute : bool = config_file.get_value("audio", "mute_music_volume", default_is_music_volume_mute)
 	
-	apply_settings(master_volume, sfx_volume, music_volume, window_mode, resolution_mode)
+	var window_mode : int = config_file.get_value("video", "window_mode", default_window_mode)
+	var resolution_mode : int = config_file.get_value("video", "resolution", default_resolution_mode)
+	
+	var language : String = config_file.get_value("language", "language", default_language)
+	
+	apply_settings(master_volume, is_master_volume_mute,
+					sfx_volume, is_sfx_volume_mute,
+					music_volume, is_music_volume_mute,
+					window_mode, resolution_mode, language)
 	
 func apply_default_setting():
-	apply_settings(50.0, 50.0, 50.0, 1, 0)
+	apply_settings(default_master_volume, default_is_master_volume_mute, 
+					default_sfx_volume, default_is_sfx_volume_mute,
+					default_music_volume, default_is_music_volume_mute,
+					default_window_mode, default_resolution_mode, default_language)
 	
-func apply_settings(master_volume: float, sfx_volume: float, music_volume: float,
-					window_mode: int, resolution: int):
+func apply_settings(master_volume: float, is_master_volume_mute: bool, 
+					sfx_volume: float, is_sfx_volume_mute: bool, 
+					music_volume: float, is_music_volume_mute: bool,
+					window_mode: int, resolution_mode: int, language : String):
 						
 	AudioServer.set_bus_volume_db(0, linear_to_db(master_volume / 100.0))
 	AudioServer.set_bus_volume_db(1, linear_to_db(sfx_volume / 100.0))
 	AudioServer.set_bus_volume_db(2, linear_to_db(music_volume / 100.0))
 	
+	AudioServer.set_bus_mute(0, is_master_volume_mute)
+	AudioServer.set_bus_mute(1, is_sfx_volume_mute)
+	AudioServer.set_bus_mute(2, is_music_volume_mute)
+	
 	apply_window_mode(window_mode)
-	apply_resolution(resolution, window_mode)
+	apply_resolution(resolution_mode, window_mode)
+	apply_language(language)
 	
 func apply_window_mode(mode_index : int):
 	match mode_index:
@@ -65,6 +97,9 @@ func apply_resolution(index : int, window_mode : int):
 	DisplayServer.window_set_size(resolutions[index])
 	center_window()
 	
+func apply_language(new_language : String):
+	TranslationServer.set_locale(new_language)
+	
 func center_window() -> void:
 	var screen: Vector2i = DisplayServer.screen_get_size()
 	var win: Vector2i = DisplayServer.window_get_size()
@@ -77,36 +112,64 @@ func center_window() -> void:
 	DisplayServer.window_set_position(center)
 	
 func get_master_volume() -> float:
-	return config_file.get_value("audio", "master_volume", 50.0)
+	return config_file.get_value("audio", "master_volume", default_master_volume)
+
+
+func get_master_volume_mute() -> bool:
+	return config_file.get_value("audio", "mute_master_volume", default_is_master_volume_mute)
 
 
 func get_sfx_volume() -> float:
-	return config_file.get_value("audio", "sfx_volume", 50.0)
+	return config_file.get_value("audio", "sfx_volume", default_sfx_volume)
+
+
+func get_sfx_volume_mute() -> bool:
+	return config_file.get_value("audio", "mute_sfx_volume", default_is_sfx_volume_mute)
 
 
 func get_music_volume() -> float:
-	return config_file.get_value("audio", "music_volume", 50.0)
+	return config_file.get_value("audio", "music_volume", default_music_volume)
+
+
+func get_music_volume_mute() -> bool:
+	return config_file.get_value("audio", "mute_music_volume", default_is_music_volume_mute)
 
 
 func get_window_mode() -> int:
-	return config_file.get_value("video", "window_mode", 1)
+	return config_file.get_value("video", "window_mode", default_window_mode)
 
 
 func get_resolution() -> int:
-	return config_file.get_value("video", "resolution", 0)
+	return config_file.get_value("video", "resolution", default_resolution_mode)
+
+
+func get_language() -> String:
+	return config_file.get_value("language", "language", default_language)
+
 	
-func save_settings(master_volume: float, sfx_volume: float, music_volume: float,
-				  window_mode: int, resolution: int):
+func save_settings(master_volume: float, is_master_volume_mute: bool, 
+					sfx_volume: float, is_sfx_volume_mute: bool, 
+					music_volume: float, is_music_volume_mute: bool,
+					window_mode: int, resolution_mode: int, language : String):
 	config_file.set_value("audio", "master_volume", master_volume)
 	config_file.set_value("audio", "sfx_volume", sfx_volume)
 	config_file.set_value("audio", "music_volume", music_volume)
 	
+	config_file.set_value("audio", "mute_master_volume", is_master_volume_mute)
+	config_file.set_value("audio", "mute_sfx_volume", is_sfx_volume_mute)
+	config_file.set_value("audio", "mute_music_volume", is_music_volume_mute)
+	
 	config_file.set_value("video", "window_mode", window_mode)
-	config_file.set_value("video", "resolution", resolution)
+	config_file.set_value("video", "resolution", resolution_mode)
+	
+	config_file.set_value("language", "language", language)
 	
 	var error: int = config_file.save(config_path)
 	if error == OK:
-		apply_settings(master_volume, sfx_volume, music_volume, window_mode, resolution)
+		apply_settings(master_volume, is_master_volume_mute,
+						sfx_volume, is_sfx_volume_mute,
+						music_volume, is_music_volume_mute,
+						window_mode, resolution_mode, language)
 	else:
 		push_error("Failed to save settings: %d" % error)
 	
--- Assets/Scripts/UI/Menus/SettingsUi.gd
+++ Assets/Scripts/UI/Menus/SettingsUi.gd
@@ -6,6 +6,11 @@ class_name SettingsUI
 @export var sfx_slider : HSlider
 @export var music_slider : HSlider
 
+@export_group("Mute Buttons")
+@export var master_mute_button : TextureButton
+@export var sfx_mute_button : TextureButton
+@export var music_mute_button : TextureButton
+
 @export_group("Text Numbers")
 @export var master_volume_text : RichTextLabel
 @export var sfx_volume_text : RichTextLabel
@@ -17,32 +22,44 @@ class_name SettingsUI
 @export var array_window_mode : Array[String]
 @export var array_resolution_mode : Array[String]
 
-var current_window_mode_index : int = 0
-var current_resolution_mode_index : int = 0
+@export_group("Language Setting")
+@export var language_toggle_button : CheckButton
 
 @export_group("Control Buttons")
 @export var apply_button : TextureButton
 @export var restore_defaults_button : TextureButton
+
+var current_window_mode_index : int = 0
+var current_resolution_mode_index : int = 0
+var current_language : String = "EN"
+
 
 func _ready() -> void:
 	load_all_user_settings()
 	
 
 func apply_and_save_settings() -> void:
-	SettingManager.save_settings(master_slider.value, 
-	sfx_slider.value, 
-	music_slider.value,
-	current_window_mode_index,
-	current_resolution_mode_index)
+	SettingManager.save_settings(
+		master_slider.value, master_mute_button.button_pressed,
+		sfx_slider.value, sfx_mute_button.button_pressed,
+		music_slider.value, music_mute_button.button_pressed,
+		current_window_mode_index, current_resolution_mode_index,
+		current_language)
 
 
 func restore_defaults() -> void:
-	var default_master_volume : float = 50.0
-	var default_sfx_volume : float = 50.0
-	var default_music_volume : float = 50.0
+	restore_default_audio_value()
+	restore_default_audio_mute()
+	restore_default_display()
+	restore_default_language()
 	
-	var default_window_mode : int = 1
-	var default_resolution : int = 0
+	apply_and_save_settings()
+
+
+func restore_default_audio_value():
+	var default_master_volume : float = SettingManager.default_master_volume
+	var default_sfx_volume : float = SettingManager.default_sfx_volume
+	var default_music_volume : float = SettingManager.default_music_volume
 	
 	master_slider.value = default_master_volume
 	sfx_slider.value = default_sfx_volume
@@ -50,20 +67,47 @@ func restore_defaults() -> void:
 	
 	update_volume_text()
 	
-	set_window_text(default_window_mode)
-	set_resolution_text(default_resolution)
 	
-	apply_and_save_settings()
+func restore_default_audio_mute():
+	var default_is_master_volume_mute : bool = SettingManager.default_is_master_volume_mute
+	var default_is_sfx_volume_mute : bool = SettingManager.default_is_sfx_volume_mute
+	var default_is_music_volume_mute : bool = SettingManager.default_is_music_volume_mute
+	
+	master_mute_button.button_pressed = default_is_master_volume_mute
+	sfx_mute_button.button_pressed = default_is_sfx_volume_mute
+	music_mute_button.button_pressed = default_is_music_volume_mute
 
+
+func restore_default_display():
+	var default_window_mode : int = SettingManager.default_window_mode
+	var default_resolution_mode : int = SettingManager.default_resolution_mode
+	
+	set_window_text(default_window_mode)
+	set_resolution_text(default_resolution_mode)
+	
+	
+func restore_default_language():
+	var default_language : String = SettingManager.default_language
+	language_toggle_button.button_pressed = false
+	
+	
 func load_all_user_settings():
 	load_audio_settings()
-	update_volume_text()
+	load_audio_mute_settings()
 	load_display_settings()
+	load_language_settings()
 	
 func load_audio_settings():
 	master_slider.value = SettingManager.get_master_volume()
 	sfx_slider.value = SettingManager.get_sfx_volume()
 	music_slider.value = SettingManager.get_music_volume()
+	update_volume_text()
+
+func load_audio_mute_settings():
+	master_mute_button.button_pressed = SettingManager.get_master_volume_mute()
+	sfx_mute_button.button_pressed = SettingManager.get_sfx_volume_mute()
+	music_mute_button.button_pressed = SettingManager.get_music_volume_mute()
+
 
 func update_volume_text():
 	master_volume_text.text = str(round(master_slider.value)) + "%"
@@ -78,6 +122,15 @@ func load_display_settings():
 	set_window_text(window_mode)
 	set_resolution_text(resolution)
 
+
+func load_language_settings():
+	var language = SettingManager.get_language()
+	if language == "EN":
+		language_toggle_button.button_pressed = false
+	else:
+		language_toggle_button.button_pressed = true
+
+
 func on_value_text_update(_value: float) -> void:
 	update_volume_text()
 
@@ -90,6 +143,7 @@ func set_window_text(index : int):
 func set_resolution_text(index : int):
 	current_resolution_mode_text.text = array_resolution_mode[index]
 	current_resolution_mode_index = index
+
 
 func on_decrease_window_mode() -> void:
 	if current_window_mode_index > 0:
